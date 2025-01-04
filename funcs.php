bfea3bf0afecf654268cde1aee9828324815d574
takanag <takanag@gmail.com>
2025-01-04 14:10:55 +0900

    Update funcs.php

--- funcs.php
+++ funcs.php
@@ -29,7 +29,7 @@ function db_conn()
     try {
         $db_name = 'takanag_db_gensen'; //データベース名
         $db_id   = 'takanag_db_gensen'; //アカウント名
-        $db_pw   = '9628Taka'; //パスワード：MAMPは'root'
+        $db_pw   = ''; //パスワード：MAMPは'root'
         $db_host = 'mysql3104.db.sakura.ne.jp'; //DBホスト
         $pdo = new PDO('mysql:dbname=' . $db_name . ';charset=utf8;host=' . $db_host, $db_id, $db_pw);
         return $pdo; //関数の外でPDO関数を使う場合にreturn $pdoとする
